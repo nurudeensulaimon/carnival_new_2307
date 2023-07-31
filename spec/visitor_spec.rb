6181eab40375ea5a0eb38ca56cd6f6706c6f78e1
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-07-31 13:00:07 -0400

    Started Iteration 2

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -9,16 +9,20 @@ RSpec.describe Visitor do
 
     describe "#initialize" do 
       it "can create a new visitor1 instance" do 
+
         expect(@visitor1).to be_a Visitor
         expect(@visitor1).to be_an_instance_of Visitor
+      
       end 
     end
 
     describe "attributes" do 
       it "has attributes and they can be read" do 
+        
         expect(@visitor1.name).to eq("Bruce")
         expect(@visitor1.height).to eq(54)
         expect(@visitor1.spending_money).to eq(10)
+        
       end
     end 
     
